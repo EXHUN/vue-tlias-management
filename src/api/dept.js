d0e89604044420e53ee62df94324ddde4c4561f6
Wang RuiQi <exhun@outlook.com>
2025-05-19 17:45:31 +0800

    更新列表查询的优化，与服务端进行异步交互

---
+++ src/api/dept.js
@@ -0,0 +1,10 @@
+import request from '../utils/request'
+
+// 查询全部部门数据
+export const queryAllApi = () => request.get('/depts');
+
+// 新增
+
+// 修改
+
+// 删除
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { queryAllApi } from "@/api/dept";
 
 // 钩子函数
 onMounted(() => {
@@ -9,9 +9,14 @@ onMounted(() => {
 
 // 查询
 const search = async () => {
-  const result = await axios.get('https://m1.apifoxmock.com/m1/6415117-6112378-default/depts');
-  if(result.data.code) { // JS隐式类型转换 0 - false ， 其它数字 - true, '' - false,其它 - true, null、undefined - false
-    deptList.value = result.data.data;
+  // const result = await axios.get('https://m1.apifoxmock.com/m1/6415117-6112378-default/depts');
+  // if(result.data.code) { // JS隐式类型转换 0 - false ， 其它数字 - true, '' - false,其它 - true, null、undefined - false
+  //   deptList.value = result.data.data;
+  // }
+
+  const result = await queryAllApi();
+  if (result.code) {
+    deptList.value = result.data;
   }
 }
 
@@ -27,13 +32,17 @@ const deptList = ref([])
 
   <div class="container">
     <el-table :data="deptList" border style="width: 100%">
-      <el-table-column type="index" label="序号"  width="100" align="center"/>
-      <el-table-column prop="name" label="部门名称" width="260" align="center"/>
-      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center"/>
+      <el-table-column type="index" label="序号" width="100" align="center" />
+      <el-table-column prop="name" label="部门名称" width="260" align="center" />
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
       <el-table-column label="操作" align="center">
         <template #default="scope">
-          <el-button type="primary" size="small"><el-icon><EditPen /></el-icon>编辑</el-button>
-          <el-button type="danger" size="small"><el-icon><Delete /></el-icon>删除</el-button>
+          <el-button type="primary" size="small"><el-icon>
+              <EditPen />
+            </el-icon>编辑</el-button>
+          <el-button type="danger" size="small"><el-icon>
+              <Delete />
+            </el-icon>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
