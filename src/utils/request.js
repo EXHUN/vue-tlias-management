3972be7113e5305a49a4dd55dc25540df6a5d981
Wang RuiQi <exhun@outlook.com>
2025-06-01 18:15:11 +0800

    更新了"登录-响应401跳转登录界面"

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { useUserStore } from '@/store/user'
+import { ElMessage } from 'element-plus'
+import router from '@/router'
+
 //创建axios实例对象
 const request = axios.create({
   baseURL: '/api',
@@ -9,19 +12,22 @@ const request = axios.create({
 
 // axios的请求 request 拦截器 - 获取LocalStorage中的token,在请求头中增加token请求头
 request.interceptors.request.use(
-  (config) => { //成功回调
-    const userStore = useUserStore()
-    const token = userStore.loginUser?.token
-    if (token) {
-      config.headers['token'] = token;
+  (config) => {
+    // 直接从 localStorage 读取
+    const piniaUser = localStorage.getItem('loginUser')
+    let token = ''
+    if (piniaUser) {
+      try {
+        token = JSON.parse(piniaUser).loginUser?.token
+      } catch {}
     }
-    
+    if (token) {
+      config.headers['token'] = token
+    }
     return config
   },
-  (error) => { //失败回调
-    return Promise.reject(error)
-  }
-) 
+  (error) => Promise.reject(error)
+)
 
 
 // axios的响应 response 拦截器
@@ -30,6 +36,18 @@ request.interceptors.response.use(
     return response.data
   },
   (error) => { //失败回调
+    if (error.response && error.response.status === 401) {
+      // 401未登录或token失效，清空pinia并跳转登录
+      // 提示信息
+      ElMessage.error('登录已过期，请重新登录')
+      const userStore = useUserStore()
+      // 清空pinia中的登录用户信息
+      userStore.loginUser = null
+      // 跳转到登录页面
+      router.push('/login')
+    }else{
+      ElMessage.error('接口访问异常')
+    }
     return Promise.reject(error)
   }
 )
